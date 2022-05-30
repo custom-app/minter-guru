8db23bc0b67ac4324143e4db8ffbce19214443c4
Лев Бакланов <penachett@bk.ru>
2022-05-30 03:55:36 +0300

    ios project init

---
+++ ios/CryptoInsta/CryptoInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoInstaApp.swift
+//  CryptoInsta
+//
+//  Created by Лев Бакланов on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
