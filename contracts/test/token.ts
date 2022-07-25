764142acbeda618900e6be9ac4cf2dd7e8a43989
Oleg Shatniuk <o.shatnuk@gmail.com>
2022-07-25 20:26:38 +0300

    vesting tests

--- contracts/test/token.ts
+++ contracts/test/token.ts
@@ -8,8 +8,9 @@ import {
   MinterGuruToken__factory,
   // eslint-disable-next-line node/no-missing-import
 } from "../typechain";
+import { before } from "mocha";
 
-describe("Token gaming event", async () => {
+describe("Token community event", async () => {
   let accounts: Signer[];
   let tokenInstance: MinterGuruToken;
   let start: number;
@@ -131,5 +132,118 @@ describe("Token gaming event", async () => {
     expect(tx)
       .to.emit("MinterGuruToken", "CommunityEventFinished")
       .withArgs(BN.from(0));
+  });
+});
+
+describe("Vesting", async () => {
+  let accounts: Signer[];
+  let tokenInstance: MinterGuruToken;
+  let start: number;
+
+  before(async () => {
+    start = Math.round(Date.now() / 1000) + 20000;
+    await ethers.provider.send("evm_mine", [start]);
+    accounts = await ethers.getSigners();
+
+    const tokenFactory = new MinterGuruToken__factory(accounts[0]);
+    tokenInstance = await tokenFactory.deploy(
+      BN.from(10000),
+      BN.from(3000),
+      BN.from(3000),
+      BN.from(4000),
+      await accounts[1].getAddress(),
+      await accounts[2].getAddress(),
+      await accounts[3].getAddress()
+    );
+  });
+
+  it("create vesting", async () => {
+    await ethers.provider.send("evm_mine", [start + 10]);
+    const tx = await tokenInstance
+      .connect(accounts[2])
+      .createVesting(
+        await accounts[0].getAddress(),
+        BN.from(10),
+        BN.from(100),
+        BN.from(10)
+      );
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingStarted")
+      .withArgs(
+        await accounts[0].getAddress(),
+        BN.from(10),
+        BN.from(100),
+        BN.from(10)
+      );
+  });
+
+  it("get vesting", async () => {
+    const record = await tokenInstance.vestingRecords(
+      await accounts[0].getAddress()
+    );
+    expect(record).deep.eq([
+      await accounts[0].getAddress(),
+      BN.from(10),
+      BN.from(100),
+      BN.from(10),
+      BN.from(start + 11),
+      BN.from(0),
+      BN.from(0),
+    ]);
+    const vestingPoolSize = await tokenInstance.vestingLeftSupply();
+    expect(vestingPoolSize).to.eq(BN.from(2900));
+  });
+
+  it("partially withdraw vesting", async () => {
+    await ethers.provider.send("evm_mine", [start + 250]);
+    const tx = await tokenInstance
+      .connect(accounts[0])
+      .withdrawVesting(BN.from(10));
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingWithdrawn")
+      .withArgs(await accounts[0].getAddress(), BN.from(10));
+  });
+
+  it("vesting withdraw too much", async () => {
+    const tx = tokenInstance.connect(accounts[0]).withdrawVesting(BN.from(30));
+    expect(tx).revertedWith(
+      "MinterGuruToken: value is greater than available amount of tokens"
+    );
+  });
+
+  it("revoke vesting", async () => {
+    const tx = await tokenInstance
+      .connect(accounts[2])
+      .revokeVesting(await accounts[0].getAddress());
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingWithdrawn")
+      .withArgs(await accounts[0].getAddress(), BN.from(10));
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingRevoked")
+      .withArgs(await accounts[0].getAddress(), BN.from(20));
+    const vestingPoolSize = await tokenInstance.vestingLeftSupply();
+    expect(vestingPoolSize).to.eq(BN.from(2980));
+  });
+
+  it("vesting with full release", async () => {
+    await ethers.provider.send("evm_mine", [start + 300]);
+    await tokenInstance
+      .connect(accounts[2])
+      .createVesting(
+        await accounts[0].getAddress(),
+        BN.from(10),
+        BN.from(100),
+        BN.from(3)
+      );
+    await ethers.provider.send("evm_mine", [start + 3000]);
+    const tx = await tokenInstance
+      .connect(accounts[0])
+      .withdrawVesting(BN.from(30));
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingWithdrawn")
+      .withArgs(await accounts[0].getAddress(), BN.from(30));
+    expect(tx)
+      .to.emit("MinterGuruToken", "VestingFullWithdrawn")
+      .withArgs(await accounts[0].getAddress(), BN.from(30));
   });
 });
